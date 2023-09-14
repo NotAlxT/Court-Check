cac6c20a11ee7b323f887ce55eb46e90905a71c3
NotAlxT <anhntran56@gmail.com>
2023-09-14 11:12:51 -0400

    v

--- components/Terms.tsx
+++ components/Terms.tsx
@@ -1,60 +1,62 @@
 const Terms = () => {
-    return ( 
+    return (
         <div>
-            Terms of Use Page:
+            <p>
+                Terms of Use Page:
 
-1. Introduction
-Welcome to CourtCheck. These Terms of Use (the "Terms") govern your use of the CourtCheck mobile app (the "App"). By using the App, you agree to be bound by these Terms.
+                1. Introduction
+                Welcome to CourtCheck. These Terms of Use (the "Terms") govern your use of the CourtCheck mobile app (the "App"). By using the App, you agree to be bound by these Terms.
 
-2. Definitions
-a. "Account" means a user account created on the App.
-b. "Content" means any information, data, text, images, videos, or other materials that are uploaded, posted, or otherwise made available through the App.
-c. "Intellectual Property Rights" means all rights, title, and interests in and to all works of authorship, including copyrights, trademarks, patents, trade secrets, and other proprietary rights.
-d. "User" means any individual who uses the App.
+                2. Definitions
+                a. "Account" means a user account created on the App.
+                b. "Content" means any information, data, text, images, videos, or other materials that are uploaded, posted, or otherwise made available through the App.
+                c. "Intellectual Property Rights" means all rights, title, and interests in and to all works of authorship, including copyrights, trademarks, patents, trade secrets, and other proprietary rights.
+                d. "User" means any individual who uses the App.
 
-3. License
-Subject to these Terms, CourtCheck grants you a non-exclusive, non-transferable, revocable license to use the App. You may use the App only for your personal, non-commercial purposes.
+                3. License
+                Subject to these Terms, CourtCheck grants you a non-exclusive, non-transferable, revocable license to use the App. You may use the App only for your personal, non-commercial purposes.
 
-4. Prohibited Use
-You may not use the App for any illegal or unauthorized purpose. You may not use the App to:
-* Upload, post, or otherwise make available any Content that is unlawful, harmful, threatening, abusive, harassing, tortious, defamatory, vulgar, obscene, or otherwise objectionable.
-* Impersonate any person or entity.
-* Interfere with or disrupt the App or the servers or networks connected to the App.
-* Violate any applicable law or regulation.
+                4. Prohibited Use
+                You may not use the App for any illegal or unauthorized purpose. You may not use the App to:
+                * Upload, post, or otherwise make available any Content that is unlawful, harmful, threatening, abusive, harassing, tortious, defamatory, vulgar, obscene, or otherwise objectionable.
+                * Impersonate any person or entity.
+                * Interfere with or disrupt the App or the servers or networks connected to the App.
+                * Violate any applicable law or regulation.
 
-5. Your Account
-You are responsible for maintaining the confidentiality of your account and password and for all activities that occur under your account. You agree to notify CourtCheck immediately of any unauthorized use of your account.
+                5. Your Account
+                You are responsible for maintaining the confidentiality of your account and password and for all activities that occur under your account. You agree to notify CourtCheck immediately of any unauthorized use of your account.
 
-6. Content
-You are solely responsible for the Content that you upload, post, or otherwise make available through the App. You grant CourtCheck a non-exclusive, worldwide, royalty-free license to use, reproduce, modify, adapt, publish, translate, distribute, and display such Content in any media or format.
+                6. Content
+                You are solely responsible for the Content that you upload, post, or otherwise make available through the App. You grant CourtCheck a non-exclusive, worldwide, royalty-free license to use, reproduce, modify, adapt, publish, translate, distribute, and display such Content in any media or format.
 
-7. Intellectual Property Rights
-The App and all Content are the property of CourtCheck or its licensors and are protected by copyright, trademark, and other intellectual property laws. You agree not to infringe on any of these intellectual property rights.
+                7. Intellectual Property Rights
+                The App and all Content are the property of CourtCheck or its licensors and are protected by copyright, trademark, and other intellectual property laws. You agree not to infringe on any of these intellectual property rights.
 
-8. Disclaimer of Warranties
-THE APP IS PROVIDED "AS IS" AND COURTCHECK EXPRESSLY DISCLAIMS ALL WARRANTIES OF ANY KIND, WHETHER EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT.
+                8. Disclaimer of Warranties
+                THE APP IS PROVIDED "AS IS" AND COURTCHECK EXPRESSLY DISCLAIMS ALL WARRANTIES OF ANY KIND, WHETHER EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT.
 
-9. Limitation of Liability
-IN NO EVENT SHALL COURTCHECK BE LIABLE FOR ANY INDIRECT, INCIDENTAL, SPECIAL, CONSEQUENTIAL, OR PUNITIVE DAMAGES, INCLUDING, BUT NOT LIMITED TO, DAMAGES FOR LOSS OF PROFITS, LOSS OF DATA, OR LOSS OF USE, EVEN IF COURTCHECK HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+                9. Limitation of Liability
+                IN NO EVENT SHALL COURTCHECK BE LIABLE FOR ANY INDIRECT, INCIDENTAL, SPECIAL, CONSEQUENTIAL, OR PUNITIVE DAMAGES, INCLUDING, BUT NOT LIMITED TO, DAMAGES FOR LOSS OF PROFITS, LOSS OF DATA, OR LOSS OF USE, EVEN IF COURTCHECK HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
 
-10. Governing Law
-These Terms will be governed by and construed in accordance with the laws of the State of California, without regard to its conflict of laws provisions.
+                10. Governing Law
+                These Terms will be governed by and construed in accordance with the laws of the State of California, without regard to its conflict of laws provisions.
 
-11. Entire Agreement
-These Terms constitute the entire agreement between you and CourtCheck with respect to the App and supersede all prior or contemporaneous communications, representations, or agreements, whether oral or written.
+                11. Entire Agreement
+                These Terms constitute the entire agreement between you and CourtCheck with respect to the App and supersede all prior or contemporaneous communications, representations, or agreements, whether oral or written.
 
-12. Severability
-If any provision of these Terms is held to be invalid or unenforceable, such provision will be struck from these Terms and the remaining provisions will remain in full force and effect.
+                12. Severability
+                If any provision of these Terms is held to be invalid or unenforceable, such provision will be struck from these Terms and the remaining provisions will remain in full force and effect.
 
-13. Changes to Terms
-CourtCheck may modify these Terms at any time. You will be notified of any changes to these Terms by email or by a notice posted on the App. Your continued use of the App after such changes have been made will constitute your acceptance of the new Terms.
+                13. Changes to Terms
+                CourtCheck may modify these Terms at any time. You will be notified of any changes to these Terms by email or by a notice posted on the App. Your continued use of the App after such changes have been made will constitute your acceptance of the new Terms.
 
-14. Contact Information
-If you have any questions about these Terms, please contact CourtCheck at:
-[Email Address]
-[Physical Address]
+                14. Contact Information
+                If you have any questions about these Terms, please contact CourtCheck at:
+                Email Address
+                Physical Address
+            </p>
         </div>
-     );
+    );
 }
- 
+
 export default Terms;
