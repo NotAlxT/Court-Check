94ad5f90fe5d41247809c95e348f037246fd318e
NotAlxT <anhntran56@gmail.com>
2023-09-17 15:34:42 -0400

    c

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -11,42 +11,52 @@ const Nav = () => {
 
             <div className="flex flex-col">
                 <div className="border border-gray-300 py-3 flex gap-1 shadow-xl rounded-md">
-                    <div className="group relative px-4 cursor-pointer">
+                    <a href="/" ><div className="group relative px-4 cursor-pointer">
+
                         <div className="flex h-10 w-10 items-center justify-center rounded-full hover:text-blue-500">
-                            <a href="/" >
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" height="32" width="32">
-                                    <path stroke="black" d="M9 22V12H15V22M3 9L12 2L21 9V20C21 20.5304 20.7893 21.0391 20.4142 21.4142C20.0391 21.7893 19.5304 22 19 22H5C4.46957 22 3.96086 21.7893 3.58579 21.4142C3.21071 21.0391 3 20.5304 3 20V9Z"></path>
-                                </svg>
-                            </a>
+
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" height="32" width="32">
+                                <path stroke="black" d="M9 22V12H15V22M3 9L12 2L21 9V20C21 20.5304 20.7893 21.0391 20.4142 21.4142C20.0391 21.7893 19.5304 22 19 22H5C4.46957 22 3.96086 21.7893 3.58579 21.4142C3.21071 21.0391 3 20.5304 3 20V9Z"></path>
+                            </svg>
+
                         </div>
+
                         <span className="absolute -bottom-8 left-[50%] -translate-x-[50%] z-20 origin-left scale-0 rounded-lg border border-gray-300 bg-black px-3 py-2 text-sm font-medium shadow-md transition-all duration-300 ease-in-out group-hover:scale-100">
                             Home
                         </span>
-                    </div>
-                    <div className="group relative px-4 cursor-pointer">
-                        <div className="flex h-10 w-10 items-center justify-center rounded-full hover:text-blue-500">
-                            <a href="/aboutUs" >
+                    </div> </a>
+                    <a href="/aboutUs" >
+                        <div className="group relative px-4 cursor-pointer">
+
+                            <div className="flex h-10 w-10 items-center justify-center rounded-full hover:text-blue-500">
+
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" height="32" width="32">
                                     <path fill="black" d="M13 9h-2V7h2m0 10h-2v-6h2m-1-9A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2Z" />
                                 </svg>
-                            </a>
+
+                            </div>
+
+                            <span className="absolute -bottom-8 left-[50%] -translate-x-[50%] z-20 origin-left scale-0 rounded-lg border border-gray-300 bg-black px-3 py-2 text-sm font-medium shadow-md transition-all duration-300 ease-in-out group-hover:scale-100">
+                                AboutUs
+                            </span>
                         </div>
-                        <span className="absolute -bottom-8 left-[50%] -translate-x-[50%] z-20 origin-left scale-0 rounded-lg border border-gray-300 bg-black px-3 py-2 text-sm font-medium shadow-md transition-all duration-300 ease-in-out group-hover:scale-100">
-                            AboutUs
-                        </span>
-                    </div>
-                    <div className="group relative px-4 cursor-pointer">
-                        <div className="flex h-10 w-10 items-center justify-center rounded-full hover:text-blue-500">
-                            <a href="/contact">
+                    </a>
+                    <a href="/contact">
+                        <div className="group relative px-4 cursor-pointer">
+
+                            <div className="flex h-10 w-10 items-center justify-center rounded-full hover:text-blue-500">
+
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" height="24" width="24">
                                     <path stroke-linejoin="round" stroke-linecap="round" stroke-width="1.5" stroke="black" d="M17 21V19C17 17.9391 16.5786 16.9217 15.8284 16.1716C15.0783 15.4214 14.0609 15 13 15H5C3.93913 15 2.92172 15.4214 2.17157 16.1716C1.42143 16.9217 1 17.9391 1 19V21M23 21V19C22.9993 18.1137 22.7044 17.2528 22.1614 16.5523C21.6184 15.8519 20.8581 15.3516 20 15.13M16 3.13C16.8604 3.3503 17.623 3.8507 18.1676 4.55231C18.7122 5.25392 19.0078 6.11683 19.0078 7.005C19.0078 7.89317 18.7122 8.75608 18.1676 9.45769C17.623 10.1593 16.8604 10.6597 16 10.88M13 7C13 9.20914 11.2091 11 9 11C6.79086 11 5 9.20914 5 7C5 4.79086 6.79086 3 9 3C11.2091 3 13 4.79086 13 7Z"></path>
                                 </svg>
-                            </a>
+
+                            </div>
+
+                            <span className="absolute -bottom-8 left-[50%] -translate-x-[50%] z-20 origin-left scale-0 rounded-lg border border-gray-300 bg-black px-3 py-2 text-sm font-medium shadow-md transition-all duration-300 ease-in-out group-hover:scale-100">
+                                Team
+                            </span>
                         </div>
-                        <span className="absolute -bottom-8 left-[50%] -translate-x-[50%] z-20 origin-left scale-0 rounded-lg border border-gray-300 bg-black px-3 py-2 text-sm font-medium shadow-md transition-all duration-300 ease-in-out group-hover:scale-100">
-                            Team
-                        </span>
-                    </div>
+                    </a>
                     <div className="group relative px-4 cursor-pointer">
                         <div className="flex h-10 w-10 items-center justify-center rounded-full hover:text-blue-500">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" height="24" width="24">
